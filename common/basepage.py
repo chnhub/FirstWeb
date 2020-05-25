f58a42a1b00bb3eb333dd3192cfda431da3398e7
chnhub <testchn@163.com>
2020-05-25 20:05:11 +0800

    使用vscode提交

--- common/basepage.py
+++ common/basepage.py
@@ -1,4 +1,16 @@
 
 class BasePage():
+
     def __init__(self):
+        pass
+    
+    
+    def open_url(self, url):
+        '''
+            传入url参数，浏览器打开url链接
+        '''
+        pass
+
+
+    def quit(self):
         pass
