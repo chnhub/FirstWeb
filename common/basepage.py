8924600afece1a2d4ef66c94e6adb1d03ee91760
userone <lipanchn@163.com>
2020-05-27 00:19:24 +0800

    整合日志框架2等

--- common/basepage.py
+++ common/basepage.py
@@ -1,9 +1,35 @@
+from common.logging import *
 
-class BasePage():
+class BasePage(object):
 
     def __init__(self):
-        pass
+        print('初始化')
+        __self__ = self
     
+    def open_brower(self, brower_type = 'chrome'):
+        '''
+            默认为Chrome浏览器，输错为Chrome浏览器。
+
+            brower_type:
+
+                chrome  -> 谷歌浏览器
+                firefox -> 火狐浏览器
+                ie      -> ie浏览器
+        '''
+        debug('调用浏览器')
+        brower_type = str(brower_type)
+        if 'chrome' in brower_type or '谷歌' in brower_type:
+            debug('使用chrome浏览器')
+            brower_type = '1'
+            
+        elif 'fire' in brower_type or '火狐' in brower_type:
+            brower_type = '2'
+        elif 'ie' in brower_type:
+            brower_type = '3'
+        else:
+            brower_type = '1'
+        
+
     
     def open_url(self, url):
         '''
---
+++ common/logging.py
@@ -0,0 +1,32 @@
+#encoding=utf-8
+import logging.config
+import logging
+import os
+print(os.path.abspath('../'))
+
+log_conf_path = 'config/logger.conf'
+logging.config.fileConfig(log_conf_path)
+logger = logging.getLogger()
+
+#日志配置文件：多个logger,每个logger，指定不同的handler
+#handler:设定了日志输出行的格式
+#handler:以及设定写日志到文件（是否回滚）？还是到屏幕
+#handler：还定了打印日志的级别。
+
+def debug(message):
+    # 打印debug级别的日志方法
+   print('debug')
+   logger.debug(message)
+
+def warning(message):
+    # 打印warning级别的日志方法
+    logger.warning(message)
+
+def info(message):
+    # 打印info级别的日志方法
+    logger.info(message)
+
+if __name__=="__main__":
+    debug("hi")
+    info("gloryroad")
+    warning("hello")
