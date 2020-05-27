23f36552f500f2b4e6eff3ab960783351f8c4253
userone <lipanchn@163.com>
2020-05-28 01:01:28 +0800

    第二次整合日志模块

--- common/basepage.py
+++ common/basepage.py
@@ -1,10 +1,12 @@
-from common.logging import *
+from common.log import *
+from selenium import webdriver
+import traceback
 
 class BasePage(object):
 
     def __init__(self):
         print('初始化')
-        __self__ = self
+        logInfo('调用浏览器')
     
     def open_brower(self, brower_type = 'chrome'):
         '''
@@ -16,10 +18,10 @@ class BasePage(object):
                 firefox -> 火狐浏览器
                 ie      -> ie浏览器
         '''
-        debug('调用浏览器')
+        logInfo('调用浏览器')
         brower_type = str(brower_type)
         if 'chrome' in brower_type or '谷歌' in brower_type:
-            debug('使用chrome浏览器')
+            logInfo('使用chrome浏览器')
             brower_type = '1'
             
         elif 'fire' in brower_type or '火狐' in brower_type:
@@ -28,7 +30,27 @@ class BasePage(object):
             brower_type = '3'
         else:
             brower_type = '1'
-        
+
+        path_driver = 'E:/Program/ChromeMaYi/guge/chromedriver'
+        try:
+            if brower_type == '1':
+                logInfo('选择谷歌浏览器')
+                driver = webdriver.Chrome(path_driver)
+                driver.get('http://www.baidu.com')
+                #time.sleep(10)
+                logInfo('倒计时结束')
+            elif brower_type == '2':
+                logInfo('选择火狐浏览器')
+            elif brower_type == '3':
+                logInfo('选择ie浏览器')
+            else:
+                logInfo('未选择')
+        except Exception as err:
+                logInfo(f'初始化浏览器失败，原因：{err}')
+                #traceback.print_exc() #直接打印异常 
+                logInfo(traceback.format_exc()) #返回字符串 
+                
+        logInfo('初始化函数结尾')
 
     
     def open_url(self, url):
--- common/log.py
+++ common/log.py
@@ -13,20 +13,21 @@ logger = logging.getLogger()
 #handler:以及设定写日志到文件（是否回滚）？还是到屏幕
 #handler：还定了打印日志的级别。
 
-def debug(message):
-    # 打印debug级别的日志方法
-   print('debug')
-   logger.debug(message)
 
-def warning(message):
+def logDebug(message):
+        # 打印debug级别的日志方法
+    print('debug')
+    logger.debug(message)
+
+def logWrning(message):
     # 打印warning级别的日志方法
     logger.warning(message)
 
-def info(message):
+def logInfo(message):
     # 打印info级别的日志方法
     logger.info(message)
 
 if __name__=="__main__":
-    debug("hi")
-    info("gloryroad")
-    warning("hello")
+    logDebug("hi")
+    logWrning("gloryroad")
+    logInfo("hello")
